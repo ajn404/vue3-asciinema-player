a9a239f6f118799478e63fabf5c9339cc864d775
ajn404 <huiyuening00@gmail.com>
2022-11-23 15:01:19 +0800

    Submit:0.0.1 not peer dependency for asciinema

--- packages/player/index.vue
+++ packages/player/index.vue
@@ -1,82 +1,37 @@
+
+
 <script setup name="player">
-import { ref, onMounted } from 'vue';
+import * as AsciinemaPlayer from 'asciinema-player';
+import 'asciinema-player/dist/bundle/asciinema-player.css'
+import { nextTick, ref } from 'vue'
+const player = ref(null)
+const themeList = ["asciinema","monokai","tango","solarized-dark","solarized-light"];//所有的主题
+const theme = themeList[Math.floor(Math.random()*10%themeList.length)]
 
-const props = defineProps({
-	starsCount: {
-		type: [Number, String],
-		default: () => 800,
-	},
-	distance: {
-		type: [Number, String],
-		default: () => 800,
-	},
-});
-
-//星星实体
-const star = ref();
-
-onMounted(() => {
-	let starArr = star.value;
-	starArr.forEach((item) => {
-		let speed = 0.1 + Math.random() * 1;
-		let thisDistance = props.distance + Math.random() * 300;
-		item.style.transformOrigin = `0 0 ${thisDistance}px`;
-		item.style.transform = `translate3d(0,0,-${thisDistance}px) rotateY(${Math.random() * 360}deg) rotateX(${
-			Math.random() * -50
-		}deg) scale(${speed},${speed})`;
-	});
+const props =defineProps({
+    file:String,
+    loop:Boolean
+})
+nextTick(() => {
+    AsciinemaPlayer.create(file||'data:text/plain;base64,eyJ2ZXJzaW9uIjogMiwgIndpZHRoIjogODAsICJoZWlnaHQiOiAyNH0KWzAuMSwgIm8iLCAiaGVsbCJdClswLjUsICJvIiwgIm8gIl0KWzIuNSwgIm8iLCAid29ybGQhXG5cciJdCg==', player.value,
+        {
+            loop: props.loop??false,
+            theme: theme
+        });
 });
 </script>
 
 <template>
-	<div class="starry-sky-bg">
-		<div class="stars">
-			<div v-for="item in starsCount" :key="item" class="star" ref="star"></div>
-		</div>
-	</div>
+    <div class="player-box">
+        <div ref="player" class="player"></div>
+    </div>
 </template>
 
 <style scoped>
-.starry-sky-bg {
-	width: 100%;
-	height: 100vh;
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: -100;
-	background: radial-gradient(200% 100% at bottom center, #f7f7b6, #e96f92, #75517d, #1b2947);
-	background: radial-gradient(220% 105% at top center, #1b2947 10%, #75517d 40%, #e96f92 65%, #f7f7b6);
-	background-attachment: fixed;
+.player :deep(.asciinema-terminal){
+    max-width: calc(100% - 3rem - 1.5em);  
 }
-
-@keyframes rotate {
-	0% {
-		transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(0);
-	}
-	100% {
-		transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(-360deg);
-	}
-}
-
-.stars {
-	transform: perspective(500px);
-	transform-style: preserve-3d;
-	position: fixed;
-	perspective-origin: 50% 100%;
-	left: 50%;
-	animation: rotate 66s infinite linear;
-	bottom: 0;
-	z-index: -99;
-}
-
-.star {
-	width: 2px;
-	height: 2px;
-	background: #f7f7b8;
-	position: fixed;
-	top: 0px;
-	left: 0;
-	backface-visibility: hidden;
+.player :deep(.asciinema-terminal) span{
+        max-width: 100%;
 }
 </style>
-
